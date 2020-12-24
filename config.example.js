6c0c8f79b3d196735df5381140d89ac6f4a94a1f
Sycrixx <biometricmc@gmail.com>
2020-12-25 05:16:47 +1100

    Add extra items with default values

--- config.example.js
+++ config.example.js
@@ -8,7 +8,17 @@ module.exports = {
     // Support Role
     support_role: "",
 
-    // Ticket Config
-    ticket_logs: "",
+    // Logging
+    log_command_executes: true,
+    ticket_log_channel: "",
+
+    // Creating Ticket
     ticket_parent: "",
+
+    // DMing User
+    dm_user: true,
+
+    // Closing Ticket
+    confirm_ticket_close: true,
+    create_transcripts: true,
 };
