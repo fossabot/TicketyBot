0a9afd4d2b6651007589b6c231f554ad2c607dd1
Sycrixx <biometricmc@gmail.com>
2020-12-25 06:30:49 +1100

    Add types to execute items

--- src/commands/close.ts
+++ src/commands/close.ts
@@ -1,23 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require("../../config");
+import { TicketyClient } from "../struct/Client";
+import { DMChannel, Message, MessageReaction, PartialUser, User } from "discord.js";
 
 export default {
     name: "close",
     description: "Closes an existing ticket",
-    async execute(client:any, message:any, args:any) {
-        if (message.channel.parentID !== config.ticket_parent) return message.channel.send("❌ This command can only be executed inside a ticket!");
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-unused-vars
+    async execute(client:TicketyClient, message:Message, args:unknown) {
+        if (!(message.channel instanceof DMChannel) && message.channel.parentID !== config.ticket_parent) return message.channel.send("❌ This command can only be executed inside a ticket!");
 
-        if (config.confirm_ticket_close === true && !message.member.roles.cache.get(config.support_role)) {
+        if (config.confirm_ticket_close === true && !message.member?.roles.cache.get(config.support_role)) {
             message.channel.send("⚠ Are you sure you want to close this ticket?").then(async (msg: { react: (arg0: string) => any; }) => {
                 await msg.react("✅");
                 await msg.react("❌");
 
-                client.once("messageReactionAdd", async (reaction: any, user: any) => {
+                client.once("messageReactionAdd", async (reaction:MessageReaction, user:User|PartialUser) => {
                     if (user.bot) return;
                     if (message.author.id !== user.id) return;
                     if (reaction.emoji.name === "✅") return await message.channel.delete();
                     if (reaction.emoji.name === "❌") return message.channel.send("✅ Aborted closing ticket!");
-                    if (reaction.count > 2) return;
+                    if (reaction.count! > 2) return;
                 });
             });
         } else {
--- src/commands/ticket.ts
+++ src/commands/ticket.ts
@@ -1,24 +1,24 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-import {logCommandExecutes} from "../utils/logCommandExecutes";
-
 const config = require("../../config");
+import { TicketyClient } from "../struct/Client";
+import { Message } from "discord.js";
+import { logCommandExecutes } from "../utils/logCommandExecutes";
 
 export default {
     name: "ticket",
     description: "Create a new ticket",
     aliases: ["new"],
-    async execute(client:any, message:any, args:any) {
+    async execute(client:TicketyClient, message:Message, args:ReadonlyArray<string>) {
         try {
             let reason = args.slice(0).join(" ");
             if (!reason || reason === "") reason = "No Reason Specified";
 
-            const ticket = await message.guild.channels.create(`ticket-${message.author.id}`, {
+            const ticket = await message.guild!.channels.create(`ticket-${message.author.id}`, {
                 parent: config.ticket_parent,
                 type: "text",
                 topic: `Reason: ${reason}`,
                 permissionOverwrites: [{
-                    id: message.guild.id,
+                    id: message.guild!.id,
                     deny: ["VIEW_CHANNEL"],
                 }, {
                     id: message.author.id,
@@ -28,7 +28,7 @@ export default {
                     allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "ADD_REACTIONS", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "USE_EXTERNAL_EMOJIS"]
                 }],
             });
-            message.channel.send(`✅ Your ticket has been created at ${ticket}`);
+            await message.channel.send(`✅ Your ticket has been created at ${ticket}`);
             logCommandExecutes(message, `Ticket ${ticket} has been created by ${message.author.tag}`);
         }
         catch (err) {
