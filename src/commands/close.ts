4f24f4aac2b96869f5d29c99568fb7c02c15de5c
Sycrixx <biometricmc@gmail.com>
2020-12-25 07:31:53 +1100

    Remove check if reaction count is greater than 2

--- src/commands/close.ts
+++ src/commands/close.ts
@@ -11,7 +11,7 @@ export default {
         if (!(message.channel instanceof DMChannel) && message.channel.parentID !== config.ticket_parent) return message.channel.send("❌ This command can only be executed inside a ticket!");
 
         if (config.confirm_ticket_close === true && !message.member?.roles.cache.get(config.support_role)) {
-            message.channel.send("⚠ Are you sure you want to close this ticket?").then(async (msg: { react: (arg0: string) => any; }) => {
+            message.channel.send("⚠ Are you sure you want to close this ticket?").then(async (msg: { react: (arg0: string) => unknown; }) => {
                 await msg.react("✅");
                 await msg.react("❌");
 
@@ -20,7 +20,6 @@ export default {
                     if (message.author.id !== user.id) return;
                     if (reaction.emoji.name === "✅") return await message.channel.delete();
                     if (reaction.emoji.name === "❌") return message.channel.send("✅ Aborted closing ticket!");
-                    if (reaction.count! > 2) return;
                 });
             });
         } else {
