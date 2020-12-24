1777a5bd083033cd89e40f5a88e4612f3fb79e66
Sycrixx <biometricmc@gmail.com>
2020-12-25 07:44:57 +1100

    Add eslint disable

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -6,6 +6,7 @@ type TicketyConfig = {
 };
 
 export class TicketyClient extends Client {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     commands = new Collection<any, any>();
     config: TicketyConfig;
 
